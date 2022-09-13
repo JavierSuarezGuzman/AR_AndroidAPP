0f1330968193ecbb08d195e62a14a4a776725be3
JavierSuarezGuzman <ja.suarezg496@gmail.com>
2022-09-13 16:21:53 -0300

    Intents OK

--- app/src/main/java/com/javiersg/proyectoandroid/LoginActivity.kt
+++ app/src/main/java/com/javiersg/proyectoandroid/LoginActivity.kt
@@ -1,11 +1,30 @@
+/* Javier Suárez Guzmán
+        Septiembre 2022 */
+
 package com.javiersg.proyectoandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginButton: Button
+    private lateinit var loginToRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton = findViewById(R.id.loginButton)
+        loginToRegister = findViewById(R.id.loginToRegister)
+
+
+        loginToRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/javiersg/proyectoandroid/RegisterActivity.kt
+++ app/src/main/java/com/javiersg/proyectoandroid/RegisterActivity.kt
@@ -1,11 +1,30 @@
+/* Javier Suárez Guzmán
+        Septiembre 2022 */
+
 package com.javiersg.proyectoandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registerButton: Button
+    private lateinit var registerToLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        registerButton = findViewById(R.id.registerButton)
+        registerToLogin = findViewById(R.id.registerToLogin)
+
+
+        registerToLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
     }
 }
