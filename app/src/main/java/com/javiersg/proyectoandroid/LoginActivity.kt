78b4128de3448fef8d4691ccf79d20c39933c89d
JavierSuarezGuzman <ja.suarezg496@gmail.com>
2022-09-14 11:44:40 -0300

    Previa cambios tutorial

--- app/src/main/java/com/javiersg/proyectoandroid/LoginActivity.kt
+++ app/src/main/java/com/javiersg/proyectoandroid/LoginActivity.kt
@@ -7,24 +7,59 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var loginEmail: EditText
+    private lateinit var loginPassword: EditText
     private lateinit var loginButton: Button
     private lateinit var loginToRegister: Button
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loginEmail = findViewById(R.id.loginEmail)
+        loginPassword = findViewById(R.id.loginPass)
         loginButton = findViewById(R.id.loginButton)
         loginToRegister = findViewById(R.id.loginToRegister)
 
-
+        //Acciones de botónes
         loginToRegister.setOnClickListener{
             startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }
+        loginButton.setOnClickListener{
+            val mail = loginEmail.text.toString()
+            val pass = loginPassword.text.toString()
+
+            if (checkEmpty(mail, pass)) {
+                //EmailAuthProvider.getCredential(mail,pass) //posiblemente una versión actualizada
+                /*auth.signInWithEmailAndPassword(mail, pass)
+                    .addOnCompleteListener(this) { task -> //tarea asíncrona
+                        if (task.isSuccessful) {*/
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                       /* } else {
+                            Toast.makeText(applicationContext, "Ingreso fallido", Toast.LENGTH_LONG).show()
+                        }
+                    }*/
+            } else {
+                Toast.makeText(applicationContext, "Rellena ambos campos", Toast.LENGTH_SHORT).show()
+
+            }
+        }
 
     }
+
+    private fun checkEmpty(mail: String, pass: String): Boolean {
+        return mail.isNotEmpty() && pass.isNotEmpty()
+    }
+
 }
--- app/src/main/java/com/javiersg/proyectoandroid/RegisterActivity.kt
+++ app/src/main/java/com/javiersg/proyectoandroid/RegisterActivity.kt
@@ -7,24 +7,76 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthRegistrar
 
 class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var registerEmail: EditText
+    private lateinit var registerPassword: EditText
+    private lateinit var registerPassRep: EditText
     private lateinit var registerButton: Button
     private lateinit var registerToLogin: Button
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        //auth = Firebase.auth //Esta declaración no funcionó
+
+        registerEmail = findViewById(R.id.registerEmail)
+        registerPassword = findViewById(R.id.registerPass)
+        registerPassRep = findViewById(R.id.registerPassRep)
         registerButton = findViewById(R.id.registerButton)
         registerToLogin = findViewById(R.id.registerToLogin)
 
-
+        //Acciones de botónes
         registerToLogin.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+        registerButton.setOnClickListener{
+            val mail = registerEmail.text.toString()
+            val pass = registerPassword.text.toString()
+            val passRep = registerPassRep.text.toString()
 
+            if(passRep.equals(pass) && checkEmpty(mail, pass, passRep)){
+                //register(mail, pass)
+
+                /*auth.createUserWithEmailAndPassword(mail,pass)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {*/
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                       /* }else {
+                            Toast.makeText(applicationContext, "Registro fallido", Toast.LENGTH_SHORT).show()
+                        }
+                    }*/
+
+            }else {
+                Toast.makeText(applicationContext, "Rellena todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    private fun checkEmpty(mail: String, pass: String, passRep: String): Boolean {
+        return mail.isNotEmpty() && pass.isNotEmpty() && passRep.isNotEmpty()
+    }
+/*
+    private fun register(mail: String, pass: String) {
+        auth.createUserWithEmailAndPassword(mail,pass)
+            .addOnCompleteListener(this) {task -> //tarea asíncrona
+                if(task.isSuccessful){
+                    //var i = Intent(this, DashboardActivity::class.java) //forma larga y nueva activity comentada
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, "Registro fallido", Toast.LENGTH_LONG).show()
+                }
+            }
+    }*/
 }
