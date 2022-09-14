c6ed7eef2b5e303f4f027c18b9a63999f0f00f5f
JavierSuarezGuzman <ja.suarezg496@gmail.com>
2022-09-14 16:05:57 -0300

    Intent Java OK!

---
+++ app/src/main/java/com/javiersg/proyectoandroid/MainActivity.java
@@ -0,0 +1,40 @@
+/* Javier Suárez Guzmán
+        Septiembre 2022 */
+
+package com.javiersg.proyectoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+//import android.widget.ArrayAdapter;
+//import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private Spinner sp1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //sp1 = (Spinner)findViewById(R.id.spinner);
+        //btn1 = (Button)findViewById(R.id.id_btn1);
+        /*
+        String [] valores = {"1. Australia","2. Dos", "3. Tres"};
+
+        ArrayAdapter <String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, valores);
+        sp1.setAdapter(adaptador);
+*/
+    }
+
+    public void Mapa(View view){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+
+}
--- app/src/main/java/com/javiersg/proyectoandroid/MainActivitykotlin.kt
+++ app/src/main/java/com/javiersg/proyectoandroid/MainActivitykotlin.kt
@@ -3,7 +3,7 @@ package com.javiersg.proyectoandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivitykotlin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
